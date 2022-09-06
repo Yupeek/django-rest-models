85ff102c25014820683b45f15e9a34717b0b60aa
Anis DAIMAR <anis.daimar@gmail.com>
2022-09-06 16:29:09 +0200

    Fix pgsql 11 credentials

--- testsettings_psql.py
+++ testsettings_psql.py
@@ -7,7 +7,7 @@ DATABASES['default'] = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'django-rest-models',
     'USER': os.environ.get('PGUSER', 'postgres'),
-    'PASSWORD': os.environ.get('DB_PWD', ''),
-    'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+    'PASSWORD': os.environ.get('PGPASSWORD', ''),
+    'HOST': os.environ.get('PGHOST', '127.0.0.1'),
     'PORT': os.environ.get('PGPORT', '5432'),
 }
