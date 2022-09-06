bf49ff5f2dd9e66117d5e00d83eb4f0a1b08e22b
Anis DAIMAR <anis.daimar@gmail.com>
2022-09-06 15:19:59 +0200

    fix testsettings_psql.py

--- testsettings_psql.py
+++ testsettings_psql.py
@@ -6,8 +6,8 @@ DATABASES = copy.deepcopy(DATABASES)
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'django-rest-models',
-    'USER': os.environ.get('DB_USER', 'postgres'),
+    'USER': os.environ.get('PGUSER', 'postgres'),
     'PASSWORD': os.environ.get('DB_PWD', ''),
     'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-    'PORT': '5432',
+    'PORT': os.environ.get('PGPORT', '5432'),
 }
