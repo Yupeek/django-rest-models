2753406e48303f6df912f056d881001e3867c4b5
Maël Saillot <mael@yupeek.com>
2024-11-29 09:21:17 +0100

    Update dependencies

--- test_runner.py
+++ test_runner.py
@@ -2,16 +2,10 @@
 import logging
 
 from django.test.runner import DiscoverRunner
-from teamcity.django import TeamcityDjangoRunner
 
 logger = logging.getLogger(__name__)
 
 
 class NoCheckDiscoverRunner(DiscoverRunner):
-    def run_checks(self, *args, **kwargs):
-        pass
-
-
-class NoCheckTeamcityDjangoRunner(TeamcityDjangoRunner):
     def run_checks(self, *args, **kwargs):
         pass
